210aad60b35b85cad3bbe6a792e7995886902b19
Semyon Perepelitsa <sema@sema.in>
2012-01-25 04:45:30 +0800

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoidRailsApp::Application.config.secret_token = 'e1709e3e03b47318499b0798859fa92b9261e375fb8ede7cd4594594b986df00ebbb446f18f232a9450603c8bbf45c3c794d694dd0376fbab3115fa893e8a5e5'
